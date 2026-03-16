8671138476b7163f953bae150ef9fcd385a92173
Sameer Saifi <Sameer.edu19@gmail.com>
2026-03-16 23:16:17 +0530

    fix(project) - fixed awaited params type in layout

--- app/(project)/p/[id]/layout.tsx
+++ app/(project)/p/[id]/layout.tsx
@@ -1,19 +1,18 @@
 import DashboardNavbar from "@/components/global/navbar";
 import ProjectNavbar from "@/components/global/project-navbar";
 import { getProjectById } from "@/server/project";
-
-interface ProjectLayoutProps {
-  children: React.ReactNode;
-  params: { id: string };
-}
+import React from "react";
 
 export default async function ProjectLayout({
   children,
   params,
-}: ProjectLayoutProps) {
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) {
   const { id } = await params;
   const { data } = await getProjectById(id);
-  console.log(data?.name)
+  console.log(data?.name);
   return (
     <>
       <div className="h-screen flex flex-col overflow-hidden">
